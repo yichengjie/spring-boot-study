a488ad5ff27a6ba091d09fadd6f8b328bda6ec97
yichengjie <626659321@qq.com>
2019-03-23 20:32:52 +0800

    使用thymeleaf正常显示页面

--- src/main/java/com/yicj/study/controller/TemplatesController.java
+++ src/main/java/com/yicj/study/controller/TemplatesController.java
@@ -1,13 +1,19 @@
 package com.yicj.study.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//现在要返回的是一个页面，所以不能再用@RestController，
+//而用普通的@Controller
+@Controller
 public class TemplatesController {
 	
 	@RequestMapping("ceshi")
-	public String ceshi() {
-		 return "ceshi";  
+	public String ceshi(HttpServletRequest req) {
+		
+		req.setAttribute("key", "hello world");
+		return "ceshi";  
 	}
 }

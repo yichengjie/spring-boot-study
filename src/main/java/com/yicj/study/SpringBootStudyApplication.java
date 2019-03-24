2772938a5f746321a2cfeb98d83ecd829a806d8f
yichengjie <626659321@qq.com>
2019-03-24 21:44:14 +0800

    spring-boot整合logback日志组件

--- src/main/java/com/yicj/study/SpringBootStudyApplication.java
+++ src/main/java/com/yicj/study/SpringBootStudyApplication.java
@@ -1,13 +1,16 @@
 package com.yicj.study;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class SpringBootStudyApplication {
-
+	static Logger logger = LoggerFactory.getLogger(SpringBootStudyApplication.class) ;
 	public static void main(String[] args) {
+		logger.info("----ready to start spring boot -------");
 		SpringApplication.run(SpringBootStudyApplication.class, args);
 	}
 

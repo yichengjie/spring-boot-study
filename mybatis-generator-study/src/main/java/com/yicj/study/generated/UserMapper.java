30582fc5e714d9432ba0270d1cd05ffabc0fb3d1
yichengjie <626659321@qq.com>
2019-08-28 17:57:07 +0800

    生成代码测试提交

---
+++ mybatis-generator-study/src/main/java/com/yicj/study/generated/UserMapper.java
@@ -0,0 +1,17 @@
+package com.yicj.study.generated;
+
+import com.yicj.study.generated.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}

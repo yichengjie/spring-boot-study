7dc7dd13bdfb5562643f853aa562754129027c40
yichengjie <626659321@qq.com>
2019-08-30 15:34:31 +0800

    关闭资源

--- mp-generator-study/src/main/java/com/yicj/study/CodeGenerator.java
+++ mp-generator-study/src/main/java/com/yicj/study/CodeGenerator.java
@@ -28,16 +28,20 @@ public class CodeGenerator {
 	 */
 	public static String scanner(String tip) {
 		Scanner scanner = new Scanner(System.in);
-		StringBuilder help = new StringBuilder();
-		help.append("请输入" + tip + "：");
-		System.out.println(help.toString());
-		if (scanner.hasNext()) {
-			String ipt = scanner.next();
-			if (StringUtils.isNotEmpty(ipt)) {
-				return ipt;
+		try {
+			StringBuilder help = new StringBuilder();
+			help.append("请输入" + tip + "：");
+			System.out.println(help.toString());
+			if (scanner.hasNext()) {
+				String ipt = scanner.next();
+				if (StringUtils.isNotEmpty(ipt)) {
+					return ipt;
+				}
 			}
+			throw new MybatisPlusException("请输入正确的" + tip + "！");
+		} finally {
+			scanner.close();
 		}
-		throw new MybatisPlusException("请输入正确的" + tip + "！");
 	}
 
 	public static void main(String[] args) {

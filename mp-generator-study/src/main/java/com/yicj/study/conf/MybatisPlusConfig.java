8580f63bccf378e24c50c7aa222791858f8a788e
yichengjie <626659321@qq.com>
2019-08-31 14:41:43 +0800

    补充性能分析插件

--- mp-generator-study/src/main/java/com/yicj/study/conf/MybatisPlusConfig.java
+++ mp-generator-study/src/main/java/com/yicj/study/conf/MybatisPlusConfig.java
@@ -2,8 +2,10 @@ package com.yicj.study.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 
 @Configuration
 public class MybatisPlusConfig {
@@ -15,5 +17,11 @@ public class MybatisPlusConfig {
         PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
         // paginationInterceptor.setLimit(你的最大单页限制数量，默认 500 条，小于 0 如 -1 不受限制);
         return paginationInterceptor;
+    }
+    //SQL执行效率插件
+    @Bean
+    @Profile({"dev","test"})// 设置 dev test 环境开启
+    public PerformanceInterceptor performanceInterceptor() {
+        return new PerformanceInterceptor();
     }
 }

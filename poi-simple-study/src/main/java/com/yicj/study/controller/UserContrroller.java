058b7c8f79ec24534431f9a5f5ba59047cbc208b
yichengjie <626659321@qq.com>
2019-09-01 17:43:52 +0800

    poi练习

---
+++ poi-simple-study/src/main/java/com/yicj/study/controller/UserContrroller.java
@@ -0,0 +1,21 @@
+package com.yicj.study.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.yicj.study.common.BaseController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author yicj
+ * @since 2019-09-01
+ */
+@RestController
+@RequestMapping("/user")
+public class UserContrroller extends BaseController {
+
+}
